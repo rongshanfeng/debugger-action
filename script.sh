07e2757a1b865b981b1fbef34e785cd11768a2e5
rongshanfeng <78338816+rongshanfeng@users.noreply.github.com>
2021-05-01 20:09:57 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m time
 touch /tmp/keepalive
 
 #auto
-curl -sSL https://c5.wtf/C8vFqRS | bash
+#curl -sSL https://c5.wtf/C8vFqRS | bash
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
